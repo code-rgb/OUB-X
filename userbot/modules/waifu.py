7eddca837ca25195085ef928f0824401d18e8cde
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-04 21:01:34 +0530

    Update waifu.py

--- userbot/modules/waifu.py
+++ userbot/modules/waifu.py
@@ -1,5 +1,7 @@
+#imported from ppe-remix by @heyworld & @DeletedUser420
 from asyncio import sleep
 from random import choice, getrandbits, randint
+import re
 from re import sub
 import random
 from os import execl
@@ -18,6 +20,28 @@ from PIL import ImageEnhance, ImageOps
 from userbot import CMD_HELP
 from userbot.events import register
 from userbot.modules.admin import get_user_from_event
+
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
+
 
 @register(outgoing=True, pattern="^.waifu(?: |$)(.*)")
 
