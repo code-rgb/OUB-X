efcb6b84eaca9e8d084cbde16714f2f7b8262a5b
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-05 14:11:24 +0530

    Update wireless_charge.py

--- userbot/modules/wireless_charge.py
+++ userbot/modules/wireless_charge.py
@@ -42,7 +42,7 @@ async def timer_blankx(e):
 
  if e.pattern_match.group(1) == 'f':
 
-  await e.edit("`Tesla Wireless Charging (beta) Completed...\nDevice Detected: Nokia 1100 [Space Grey Varient]\nBattery Percentage:` [100%] https://telegra.ph/file/93a3563e501394fb1e861.mp4 ", link_preview=True)
+  await e.edit("`Tesla Wireless Charging (beta) Completed...\nDevice Detected: Nokia 1100` Space Grey Varient\n`Battery Percentage:` [100%] https://telegra.ph/file/93a3563e501394fb1e861.mp4 ", link_preview=True)
 
     
     
