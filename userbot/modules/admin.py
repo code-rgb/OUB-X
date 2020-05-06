30727ac06da051c49615e0e152486cc19c89d784
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-06 17:10:31 +0530

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -946,34 +946,34 @@ async def get_bots(show):
 
 CMD_HELP.update({
     "admin":
-    ".𝗽𝗿𝗼𝗺𝗼𝘁𝗲 <username/reply> <custom rank (optional)>\
+    ".promote <username/reply> <custom rank (optional)>\
 \nUsage: Provides admin rights to the person in the chat.\
-\n\n.𝗱𝗲𝗺𝗼𝘁𝗲 <username/reply>\
+\n\n.demote <username/reply>\
 \nUsage: Revokes the person's admin permissions in the chat.\
-\n\n.𝗯𝗮𝗻 <username/reply> <reason (optional)>\
+\n\n.ban <username/reply> <reason (optional)>\
 \nUsage: Bans the person off your chat.\
-\n\n.𝘂𝗻𝗯𝗮𝗻 <username/reply>\
+\n\n.unban <username/reply>\
 \nUsage: Removes the ban from the person in the chat.\
-\n\n.𝗺𝘂𝘁𝗲 <username/reply> <reason (optional)>\
+\n\n.mute <username/reply> <reason (optional)>\
 \nUsage: Mutes the person in the chat, works on admins too.\
-\n\n.𝘂𝗻𝗺𝘂𝘁𝗲 <username/reply>\
+\n\n.unmute <username/reply>\
 \nUsage: Removes the person from the muted list.\
-\n\n.𝗴𝗺𝘂𝘁𝗲 <username/reply> <reason (optional)>\
+\n\n.gmute <username/reply> <reason (optional)>\
 \nUsage: Mutes the person in all groups you have in common with them.\
-\n\n.𝘂𝗻𝗴𝗺𝘂𝘁𝗲 <username/reply>\
+\n\n.ungmute <username/reply>\
 \nUsage: Reply someone's message with .ungmute to remove them from the gmuted list.\
-\n\n.𝘇𝗼𝗺𝗯𝗶𝗲𝘀\
+\n\n.zombies\
 \nUsage: Searches for deleted accounts in a group. Use .zombies clean to remove deleted accounts from the group.\
-\n\n.𝗮𝗱𝗺𝗶𝗻𝘀\
+\n\n.admins\
 \nUsage: Retrieves a list of admins in the chat.\
-\n\n.𝗯𝗼𝘁𝘀\
+\n\n.bots\
 \nUsage: Retrieves a list of bots in the chat.\
-\n\n.𝗽𝗶𝗻 <reply/tag>\
+\n\n.pin <reply/tag>\
 \nUsage: pins the replied/tagged message on the top the chat silently.\
-\n\n.𝗰𝗽𝗶𝗻 <reply/tag>\
+\n\n.cpin <reply/tag>\
 \nUsage: pins the replied/tagged message on the top the chat LOUDLY.\
-\n\n.𝘂𝘀𝗲𝗿𝘀 or .users <name of member>\
+\n\n.users or .users <name of member>\
 \nUsage: Retrieves all (or queried) users in the chat.\
-\n\n.𝘀𝗲𝘁𝗴𝗽𝗽𝗶𝗰 <reply to image>\
+\n\n.setgppic <reply to image>\
 \nUsage: Changes the group's display picture."
 })
