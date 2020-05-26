d90e008433681a2e41ec81c1db12f0f030333bea
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-26 11:46:07 +0530

    Update warns.py

--- userbot/modules/warns.py
+++ userbot/modules/warns.py
@@ -1,69 +1,62 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# Ported from SpEcHiDe UniBorg by github.com/HitaloKun/TG-UBotX
-
-
+import asyncio
 import html
-
-from userbot import CMD_HELP
+import logging
+from telethon import events
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+from userbot import CMD_HELP, bot
 from userbot.events import register
 import userbot.modules.sql_helper.warns_sql as sql
 
 
-@register(outgoing=True, pattern=r"^\.warn (.*)")
+
+
+@register(outgoing=True, pattern="^.warn(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     warn_reason = event.pattern_match.group(1)
     reply_message = await event.get_reply_message()
     limit, soft_warn = sql.get_warn_setting(event.chat_id)
-    num_warns, reasons = sql.warn_user(
-        reply_message.from_id, event.chat_id, warn_reason)
+    num_warns, reasons = sql.warn_user(reply_message.from_id, event.chat_id, warn_reason)
     if num_warns >= limit:
         sql.reset_warns(reply_message.from_id, event.chat_id)
         if soft_warn:
-            logger.info("TODO: kick user")
-            reply = "<code>{}</code> warnings, <u><a href='tg://user?id={}'>user</a></u> has been kicked!".format(
-                limit, reply_message.from_id)
+            logging.info("TODO: kick user")
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been kicked!".format(limit, reply_message.from_id)
         else:
-            logger.info("TODO: ban user")
-            reply = "<code>{}</code> warnings, <u><a href='tg://user?id={}'>user</a></u> has been banned!".format(
-                limit, reply_message.from_id)
+            logging.info("TODO: ban user")
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been banned!".format(limit, reply_message.from_id)
     else:
-        reply = "<u><a href='tg://user?id={}'>User</a></u> has <code>{}/{}</code> warnings... watch out!".format(
-            reply_message.from_id, num_warns, limit)
+        reply = "<u><a href='tg://user?id={}'>user</a></u> has {}/{} warnings... watch out!".format(reply_message.from_id, num_warns, limit)
         if warn_reason:
-            reply += "\nReason for last warn:\n<code>{}</code>".format(
-                html.escape(warn_reason))
+            reply += "\nReason for last warn:\n{}".format(html.escape(warn_reason))
     #
     await event.edit(reply, parse_mode="html")
 
 
-@register(outgoing=True, pattern=r"^\.warns$")
+@register(outgoing=True, pattern="^.getwarns(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     reply_message = await event.get_reply_message()
+    logger= None
     result = sql.get_warns(reply_message.from_id, event.chat_id)
     if result and result[0] != 0:
         num_warns, reasons = result
         limit, soft_warn = sql.get_warn_setting(event.chat_id)
         if reasons:
-            text = "This user has `{}/{}` warnings, for the following reasons:".format(
-                num_warns, limit)
-            text += "\n"
+            text = "This user has {}/{} warnings, for the following reasons:".format(num_warns, limit)
+            text += "\r\n"
             text += reasons
             await event.edit(text)
         else:
-            await event.edit("This user has `{}/{}` warning, but no reasons for any of them.".format(num_warns, limit))
+            await event.edit("This user has {} / {} warning, but no reasons for any of them.".format(num_warns, limit))
     else:
         await event.edit("This user hasn't got any warnings!")
 
 
-@register(outgoing=True, pattern=r"^\.resetwarns$")
+@register(outgoing=True, pattern="^.resetwarn(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -71,11 +64,3 @@ async def _(event):
     sql.reset_warns(reply_message.from_id, event.chat_id)
     await event.edit("Warnings have been reset!")
 
-
-CMD_HELP.update({
-    "warns": ".warn \nUsage : Warn a user\
-    \n\n.warns\
-    \nUsage: See all warns for a user\
-    \n\n.resetwarns\
-    \nUsage: Reset all warns of a user"
-})
