3c1947403840c53e37a631f8374a29b1af8ef4b6
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-27 20:08:25 +0530

    Update and rename new_help.py to Mlist.py

--- userbot/modules/Mlist.py
+++ userbot/modules/Mlist.py
@@ -1,7 +1,3 @@
-from userbot.events import register
-from telethon import events
-import asyncio
-
 
 # @register(outgoing=True, pattern="^.helpx$")
 # async def helpx(e):
@@ -40,12 +36,16 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.listhelp(?: |$)(.*)")
+@register(outgoing=True, pattern="^.mlist(?: |$)(.*)")
 async def help(event):
-            string = ""
+    """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+        string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`  -  "
         await event.reply(string)
-
- 
