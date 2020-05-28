12da834b3da01a29b4061028f497f92e03facf09
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-05-28 20:49:34 +0530

    Update Mlist.py

--- userbot/modules/Mlist.py
+++ userbot/modules/Mlist.py
@@ -1,14 +1,22 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
+
 from userbot import CMD_HELP
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.mlist(?: |$)(.*)")
+
+@register(outgoing=True, pattern="^.mlist$")
 async def help(event):
     """ For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
-        else: await event.edit("List Of Installed modules use .help<module name>")
+            await event.edit("**Installed Modules**😎\
+            \nUsage: Type `.help <module name>` to know how it works✌️")
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
